be67062cc821cfebf788739ceef3a0c4c2c94ae2
Ajith Nair <ajith.nair@slalom.com>
2025-03-12 11:10:07 -0400

    add third function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const secondFunc = () => {
     console.log('Hello from second function');
 };
 
+const thridFunc = () => {
+    console.log('Hello from the third function');
+};
+
 export const firstModule = () => {
     console.log('Hello from first module');
     secondFunc();
